3be202c77266e3b7bda587ed39182543c849873f
chiranjeevi_gorantla <chiranjeevigorantla@gmail.com>
2023-01-25 15:10:31 -0800

    Initial commit

---
+++ src/main/java/com/chirango/SpringBootExplore/SpringBootExploreApplication.java
@@ -0,0 +1,14 @@
+package com.chirango.SpringBootExplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExploreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootExploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chirango/SpringBootExplore/SpringBootExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirango.SpringBootExplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
